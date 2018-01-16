0dcf30f5ac6711d61273c9c28736c805536f5a4f
Abhinav <abhinav1@istarindia.com>
2018-01-16 18:08:26 +0530

    XML downloaded from Business

---
+++ presentationUI/PresentationLesson.swift
@@ -0,0 +1,9 @@
+//
+//  PresentationLesson.swift
+//  presentationUI
+//
+//  Created by absin on 1/16/18.
+//  Copyright © 2018 absin.io. All rights reserved.
+//
+
+import Foundation
--- presentationUI/ViewController.swift
+++ presentationUI/ViewController.swift
@@ -20,6 +20,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonAction(_ sender: Any) {
+        let documentsUrl:URL =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first as URL!
+        let destinationFileUrl = documentsUrl.appendingPathComponent("163.xml")
+        let fileURL = URL(string: "http://business.talentify.in:9999/lessonXMLs/163/163/163.xml")
+        
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+        
+        let request = URLRequest(url:fileURL!)
+        
+        if UIApplication.shared.canOpenURL(destinationFileUrl){
+            do {
+                let text = try String(contentsOf: destinationFileUrl)
+                print(text)
+            } catch {
+                print("Cant read")
+            }
+        } else {
+            let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
+                if let tempLocalUrl = tempLocalUrl, error == nil {
+                    // Success
+                    if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                        print("Successfully downloaded. Status code: \(statusCode)")
+                    }
+                    
+                    do {
+                        try FileManager.default.copyItem(at: tempLocalUrl, to: destinationFileUrl)
+                    } catch (let writeError) {
+                        print("Error creating a file \(destinationFileUrl) : \(writeError)")
+                    }
+                    
+                } else {
+                    print("Error took place while downloading a file. Error description: %@", error?.localizedDescription);
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    
 }
 
